98b4b52bc38497b6b1cd94c6fe9d9d7fcf7ab09c
pnkuria <pnkuria001@gmail.com>
2023-04-05 09:52:53 +0300

    changed

---
+++ kirk-java/index.js
@@ -0,0 +1,30 @@
+let num = 9
+if (num>10){
+    console.log("number is greater than 10")
+} else{
+    console.log("number is less than 10 ")
+}
+
+let num1 = 35
+if (num==10){
+    console.log("num is 10")
+}else if(num>10){
+    console.log("num is greater than 10")
+}
+
+let continent = "Africa"
+let age = 19
+/*if (continent=="North-America"){
+    if (age>=18){
+        console.log("Accepted")
+    }else{
+      console.log("Not accepted")
+    }
+}else{
+  console.log("Not accepted")
+}*/
+if (continent=="North-America" && age>=18){
+    console.log("Accepted")
+}else{
+    console.log("Not Accepted")
+}
